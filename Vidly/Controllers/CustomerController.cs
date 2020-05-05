da6143e8682f609da213df23eb787ffe2ae61be4
Isaac Clement <isaacclem7@gmail.com>
2020-05-05 03:30:22 +0100

    first commit
    first commit

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        CustomerDbContext context = new CustomerDbContext();
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(context.customers.ToList());  
+        }
+    }
+}
---
+++ Vidly/Models/CustomerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class CustomerDbContext: DbContext
+    {
+        public DbSet<Customer> customers { get; set; }
+    }
+}
